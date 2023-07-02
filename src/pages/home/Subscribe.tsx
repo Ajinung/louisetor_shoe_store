55bd109b5eb236aebdda0e5951bf7c4e687f7167
Isaac Etor <114070166+Ajinung@users.noreply.github.com>
2023-07-02 20:14:19 +0100

    fixed the subscrbe inout

--- src/pages/home/Subscribe.tsx
+++ src/pages/home/Subscribe.tsx
@@ -4,39 +4,43 @@ const Subscribe = () => {
   const [isFocused, setIsFocused] = useState(false);
 
   return (
-    <div className="w-full h-[35vh] bg-gray-100 flex items-center justify-center flex-col">
-      <h1 className="font-semibold text-lg">Enter The World Of Louis Etor</h1>
-      <p className="text-gray-600">Hear about latest designs and more.</p>
+    <div className="w-full py-12 bg-gray-100 flex items-center justify-center ">
+      <div className="w-fit h[50px]  flex items-center flex-col">
+        <h1 className="font-semibold text-lg">Enter The World Of Louis Etor</h1>
+        <p className="text-gray-600">Hear about latest designs and more.</p>
 
-      {/* input */}
-      <div className="mt-5 bg-black w-[20%] h-[40px] flex relative max-md:w-[50%]">
-        <input
-          type="email"
-          required
-          className={`flex-1 px-3 outline-none border-[1px] text-gray-400 ${
-            isFocused
-              ? "border-purple-500 ease-in duration-300"
-              : "ease-in duration-300"
-          }`}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-        />
-        <div
-          className={`absolute px-3 bottom-3 left-3 ease-in-out ${
-            isFocused ? "-translate-y-6 opacity-0" : "translate-y-0 opacity-100"
-          } text-slate-400 text-sm pointer-events-none`}
-        >
-          email address
+        {/* input */}
+        <div className="mt-5 bg-black w-full h-[40px] flex relative">
+          <input
+            type="email"
+            required
+            className={`flex-1 px-3 outline-none border-[1px] text-gray-400 ${
+              isFocused
+                ? "border-purple-500 ease-in duration-300"
+                : "ease-in duration-300"
+            }`}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+          />
+          <div
+            className={`absolute px-3 bottom-3 left-3 ease-in-out ${
+              isFocused
+                ? "-translate-y-6 opacity-0"
+                : "translate-y-0 opacity-100"
+            } text-slate-400 text-sm pointer-events-none`}
+          >
+            email address
+          </div>
+          <p
+            className={`absolute px-3 bottom-[-25px] transition-all duration-500 transform ${
+              isFocused
+                ? "translate-y-0 opacity-100"
+                : "-translate-y-full opacity-0"
+            } z-10 text-purple-500 text-sm`}
+          >
+            email address
+          </p>
         </div>
-        <p
-          className={`absolute px-3 bottom-[-25px] transition-all duration-500 transform ${
-            isFocused
-              ? "translate-y-0 opacity-100"
-              : "-translate-y-full opacity-0"
-          } z-10 text-purple-500 text-sm`}
-        >
-          email address
-        </p>
       </div>
     </div>
   );
